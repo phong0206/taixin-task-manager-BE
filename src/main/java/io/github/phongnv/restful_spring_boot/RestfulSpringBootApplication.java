0ae0aeca2611e1be2dba5e31f6d3b65dc3006451
phong0206 <vanphong02062002@gmail.com>
2025-02-05 14:46:20 +0700

    init base

---
+++ src/main/java/io/github/phongnv/restful_spring_boot/RestfulSpringBootApplication.java
@@ -0,0 +1,14 @@
+package io.github.phongnv.restful_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication()
+@EnableJpaAuditing
+public class RestfulSpringBootApplication {
+    public static void main(String[] args) {
+        System.out.println("✅ Ứng dụng Spring Boot đã chạy thành công!");
+        SpringApplication.run(RestfulSpringBootApplication.class, args);
+    }
+}
---
+++ src/test/java/io/github/phongnv/restful_spring_boot/RestfulSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.phongnv.restful_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
